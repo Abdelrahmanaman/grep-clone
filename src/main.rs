5e1c4032a397484acce528beaba676cd1763ac0a
abdelrahmanaman <abdelrahman.bourka1@gmail.com>
2025-03-09 17:32:21 +0100

    feat: add struct for cli config

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,23 @@
-use std::fs::File;
-use std::io::{self, BufRead, BufReader};
-fn main() -> io::Result<()>{
+use std::{env, fs};
+fn main() {
+    //Geting args from the CLI
+    let args: Vec<String> = env::args().collect();
 
-    let file = File::open("foo.txt")?;
+    let Config { query, file_path } = parse_config(&args);
+    print!("{} {}", query, file_path);
 
-    let reader = BufReader::new(file);
+    let content = fs::read_to_string(file_path).expect("Something went wrong reading the file");
 
-    for line in reader.lines() {
-        let line = line?;
-        println!("{}", line);
-
-    }
-
-
-    
-
-
-    Ok(())
+    println!("\n{}", content);
 }
 
+struct Config {
+    query: String,
+    file_path: String,
+}
+fn parse_config(args: &Vec<String>) -> Config {
+    let query: String = args[1].clone();
+    let file_path: String = args[2].clone();
+
+    Config { query, file_path }
+}
