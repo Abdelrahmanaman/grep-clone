640f2ada81523096afdad3ee3e83c988b8da6acc
abdelrahmanaman <abdelrahman.bourka1@gmail.com>
2025-03-09 23:54:07 +0100

    feat: add a color highlight for query

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
+use ansi_term::Color;
 use std::{error::Error, fs};
-
 pub struct Config {
     pub query: String,
     pub file_path: String,
@@ -19,22 +19,28 @@ impl Config {
     pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
         let content = fs::read_to_string(config.file_path)?;
 
-        for line in search(&config.query, &content) {
-            println!("Query found: {}", line)
+        for (line_num, line) in search(&config.query, &content) {
+            let highlighted = line.replace(
+                &config.query,
+                &Color::Yellow.bold().paint(&config.query).to_string(),
+            );
+            println!("Query found in line {}: {}", line_num, highlighted);
         }
-
         Ok(())
     }
 }
 
-pub fn search<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
-    let mut results: Vec<&str> = Vec::new();
+pub fn search<'a>(query: &str, content: &'a str) -> Vec<(i32, &'a str)> {
+    let mut results: Vec<(i32, &'a str)> = Vec::new();
+    let mut line_num = 1;
 
     for line in content.lines() {
         if line.contains(query) {
-            results.push(line.trim());
+            results.push((line_num, line.trim()));
         }
+        line_num += 1;
     }
+
     results
 }
 
