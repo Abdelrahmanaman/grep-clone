47be924bc01913db8e52d621c28271665a5d9e21
abdelrahmanaman <abdelrahman.bourka1@gmail.com>
2025-03-09 23:09:36 +0100

    feat: add test

---
+++ src/lib.rs
@@ -0,0 +1,50 @@
+use std::fs;
+
+pub struct Config {
+    pub query: String,
+    pub file_path: String,
+}
+
+impl Config {
+    pub fn build(args: &Vec<String>) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            panic!("Not enough arguments")
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config { query, file_path })
+    }
+    pub fn run(config: Config) -> Result<(), &'static str> {
+        let content = fs::read_to_string(config.file_path).expect("So");
+        let query = config.query;
+        println!("The query {} in the file {}", query, content);
+
+        Ok(())
+    }
+}
+
+pub fn search<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
+    let mut results: Vec<&str> = Vec::new();
+    for line in content.lines() {
+        if line.contains(query) {
+            results.push(line.trim());
+        }
+    }
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn find_result() {
+        let query = "duct";
+        let content = "\
+        Rust:
+        safe, fast, productive.
+        Pick three.";
+        assert_eq!(vec!["safe, fast, productive."], search(query, content))
+    }
+}
